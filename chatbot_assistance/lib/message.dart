f0d3d0ba3dc779876e86d4e30a1ae484ce526c04
Amantha_Abeykoon <139742416+BACAbeykoon@users.noreply.github.com>
2025-05-20 00:23:59 +0530

    Update message.dart

--- chatbot_assistance/lib/message.dart
+++ chatbot_assistance/lib/message.dart
@@ -10,6 +10,6 @@ class MessageBubble extends StatefulWidget {
 class _MessageBubbleState extends State<MessageBubble> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: AppBar(title: Text("Message Bubble")));
   }
 }
